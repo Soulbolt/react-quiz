8e764054d905e02c27637b5b1c70cc44ae11a7a3
Soulbolt <soulbolt@gmail.com>
2024-02-08 13:26:34 -0700

    Add Question.js file to display questions. Add index to initial state. Add active status conditional to switch case. Add index porperty to reducer variable. add new conditional to display question on lets get start button click.

--- src/Question.js
+++ src/Question.js
@@ -1,5 +1,10 @@
-function Question() {
-  return <div>Question</div>;
+function Question({ question }) {
+  console.log(question);
+  return (
+    <div>
+      <h4>{question.question}</h4>
+    </div>
+  );
 }
 
 export default Question;
