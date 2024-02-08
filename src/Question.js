a6789c9bfadbd9ac22a776623bcfa63a6c3238d4
Soulbolt <soulbolt@gmail.com>
2024-02-08 13:36:45 -0700

    Add iteration to display options into a selection buttons.

--- src/Question.js
+++ src/Question.js
@@ -3,6 +3,13 @@ function Question({ question }) {
   return (
     <div>
       <h4>{question.question}</h4>
+      <div className="options">
+        {question.options.map((options) => (
+          <button className="btn btn-option" key={options}>
+            {options}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
